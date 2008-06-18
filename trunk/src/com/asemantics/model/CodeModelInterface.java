32d59e775ca18d6889f8f7e04286b4841b49a985
michele.mostarda <michele.mostarda@3448afda-2c48-0410-b2fa-1f034b7baa63>
2008-06-18 20:59:05 +0000

    Added ValidatingCodeModel, introduced CodeModelInterface.

---
+++ trunk/src/com/asemantics/model/CodeModelInterface.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2007-2008 Michele Mostarda ( michele.mostarda@gmail.com ).
+ * All Rights Reserved.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.asemantics.model;
+
+/**
+ * Defines the operations exposed by a generic Code Model.
+ */
+public interface CodeModelInterface {
+
+    /**
+     * Allows to perform a triple search on the org.asemantics.model.
+     * @param subject
+     * @param predicate
+     * @param object
+     * @return
+     */
+    TripleIterator searchTriples(String subject, String predicate, String object);
+
+    /**
+     * Allows to add a triple into the model.
+     * NOTE: this method accepts only resource objects.
+     *
+     * @param subject a subject resource
+     * @param predicate a predicate
+     * @param object a resource object
+     */
+    void addTriple(String subject, String predicate, String object);
+
+    /**
+     * Allows to remove a triple from the model.
+     * NOTE: this method accepts only resource objects.
+     *
+     * @param subject
+     * @param predicate
+     * @param object
+     * @see #addTriple(String, String, String)
+     */
+    void removeTriple(String subject, String predicate, String object);
+
+    /**
+     * Allows to add a triple into the model.
+     * NOTE: this method accepts only literal object.
+     *
+     * @param subject a subject resource
+     * @param predicate a predicate
+     * @param literal a literal object
+     */
+    void addTripleLiteral(String subject, String predicate, String literal);
+
+    /**
+     * Allows to remove a triple from the model.
+     * NOTE: this method accepts only literal object.
+     *
+     * @param subject
+     * @param predicate
+     * @param object
+     * @see #addTripleLiteral(String, String, String)
+     */
+    void removeTripleLiteral(String subject, String predicate, String object);
+
+    /**
+     *  Remove the entire content of the model.
+     */
+    void clearAll();
+}
---
+++ trunk/src/com/asemantics/model/ontology/ValidatingCodeModel.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2007-2008 Michele Mostarda ( michele.mostarda@gmail.com ).
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.asemantics.model.ontology;
+
+import com.asemantics.model.CodeModelInterface;
+import com.asemantics.model.TripleIterator;
+
+/**
+ * This decorator validates addition of model triples over a given
+ * ontology.
+ *
+ * @see com.asemantics.model.CodeModelInterface
+ * @see com.asemantics.model.ontology.Ontology
+ */
+public class ValidatingCodeModel implements CodeModelInterface {
+
+    /**
+     * Validation error message.
+     */
+    private static String VALIDATION_ERROR = "Validation error";
+
+    /**
+     * Decorated code model.
+     */
+    private CodeModelInterface decorated;
+
+    /**
+     * Applied ontology.
+     */
+    private Ontology ontology;
+
+    public ValidatingCodeModel(CodeModelInterface codeModel, Ontology otlg) {
+        if(codeModel == null || otlg == null) {
+            throw new IllegalArgumentException();
+        }
+
+        decorated = codeModel;
+        ontology  = otlg;
+    }
+
+    public TripleIterator searchTriples(String subject, String predicate, String object) {
+        return decorated.searchTriples(subject, predicate, object);
+    }
+
+    public void addTriple(String subject, String predicate, String object) {
+        try {
+            ontology.validateTriple(subject, predicate, object);
+        } catch (OntologyException oe) {
+            throw new RuntimeException(VALIDATION_ERROR, oe);
+        }
+        decorated.addTriple(subject, predicate, object);
+    }
+
+    public void removeTriple(String subject, String predicate, String object) {
+        decorated.removeTriple(subject, predicate, object);
+    }
+
+    public void addTripleLiteral(String subject, String predicate, String literal) {
+        try {
+            ontology.validateTripleLiteral(subject, predicate);
+        } catch (OntologyException oe) {
+            throw new RuntimeException(VALIDATION_ERROR, oe);
+        }
+        decorated.addTripleLiteral(subject, predicate, literal);
+    }
+
+    public void removeTripleLiteral(String subject, String predicate, String object) {
+        decorated.removeTripleLiteral(subject, predicate, object);
+    }
+
+    public void clearAll() {
+        decorated.clearAll();
+    }
+}
