b1447f38d05e4ff92d91382588fe56b77acebc08
michele.mostarda <michele.mostarda@3448afda-2c48-0410-b2fa-1f034b7baa63>
2008-07-29 17:35:54 +0000

    Added specific tests to QueryModel and JavaQeuryModel.

---
+++ test/com/asemantics/model/QueryModelTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2007-2008 Michele Mostarda ( michele.mostarda@gmail.com ).
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.asemantics.model;
+
+import junit.framework.TestCase;
+import com.asemantics.sourceparse.JStatistics;
+import com.asemantics.sourceparse.JavaBytecodeJarParser;
+import com.asemantics.sourceparse.ObjectsTable;
+import com.asemantics.storage.JenaCoderFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class QueryModelTest extends TestCase {
+
+    private CodeModelBase codeModelBase;
+
+    private JavaQueryModel javaQueryModel;
+
+    /**
+     * Creates a query model by parsing target.jar.
+     * @throws IOException
+     */
+    private CodeModelBase createQueryModel() throws IOException {
+        JStatistics statistics = new JStatistics();
+        CoderFactory coderFactory = new JenaCoderFactory();
+        CodeModelBase codeModelBase = coderFactory.createCodeModel();
+        CodeHandler codeHandler = coderFactory.createHandlerOnModel( codeModelBase);
+
+        JavaBytecodeJarParser parser = new JavaBytecodeJarParser();
+        CodeHandler statCH = statistics.createStatisticsCodeHandler(codeHandler);
+        ObjectsTable objectsTable = new ObjectsTable();
+        parser.initialize( statCH, objectsTable );
+
+        File lib = new File("target_test/target.jar");
+
+        codeHandler.startParsing("test_model", lib.getAbsolutePath());
+        parser.parseFile( lib );
+        codeHandler.endParsing();
+
+        statistics.reset();
+        parser.dispose();
+        parser = null;
+
+        return codeModelBase;
+    }
+
+    public QueryModelTest() throws IOException {
+        codeModelBase = createQueryModel();
+    }
+
+    public void testQueryModel() {
+        final String EXPECTED_LIBRARY = "asset:test_model";
+
+        QueryModel queryModel = javaQueryModel;
+
+        Asset asset = queryModel.getAsset();
+        assertNotNull(asset);
+
+        String[] libraries = asset.getLibraries();
+        assertNotNull(libraries);
+        assertTrue( libraries.length == 1);
+        assertEquals("Wrong library name.", libraries[0], EXPECTED_LIBRARY);
+
+        assertNotNull("Cannot retrieve library date",  queryModel.getLibraryDateTime(EXPECTED_LIBRARY));
+
+        assertNotNull("Cannot retrieve library location",  queryModel.getLibraryLocation(EXPECTED_LIBRARY));
+    }
+
+    protected void setUp() throws Exception {
+         javaQueryModel = new JavaQueryModelImpl(codeModelBase);
+
+    }
+
+    protected void tearDown() throws Exception {
+        javaQueryModel = null;
+    }
+
+    protected JavaQueryModel getJavaQueryModel() {
+        return javaQueryModel;
+    }
+
+}
